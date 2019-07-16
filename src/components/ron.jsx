fb424d3c9a261b656a435cbdb87ea6ef5c5d7b6c
Darren Inglis <darren.inglis@gmail.com>
2019-07-17 08:21:02 +1000

    cleaned up sound list and replaced random sounds with serial sounds

--- src/components/ron.jsx
+++ src/components/ron.jsx
@@ -9,13 +9,11 @@ import steak from '../images/steak.svg';
 // sounds
 import giggle from '../sounds/ron_swanson_giggle.mp3';
 import joke from '../sounds/tax_joke.mp3';
-import breakfast from '../sounds/breakfast.mp3';
 import at_doctor from '../sounds/at_doctor.mp3';
 import bacon_and_eggs from '../sounds/bacon_and_eggs.mp3';
 import beaurocrat from '../sounds/beaurocrat.mp3';
 import born_ready from '../sounds/born_ready.mp3';
 import cake from '../sounds/cake.mp3';
-import capitalisim from '../sounds/capitalisim.mp3';
 import chummy from '../sounds/chummy.mp3';
 import currency from '../sounds/currency.mp3';
 import dentist from '../sounds/dentist.mp3';
@@ -25,83 +23,72 @@ import frozen_yoghurt from '../sounds/frozen_yoghurt.mp3';
 import hangover from '../sounds/hangover.mp3';
 import hernia from '../sounds/hernia.mp3';
 import honeymoon from '../sounds/honeymoon.mp3';
-import hostile_witness from '../sounds/hostile_witness.mp3';
 import i_know_more_than_you from '../sounds/i_know_more_than_you.mp3';
-import no_steak from '../sounds/no_steak.mp3';
 import party_platter from '../sounds/party_platter.mp3';
 import ribs from '../sounds/ribs.mp3';
-import salad from '../sounds/salad.mp3';
 import say_no from '../sounds/say_no.mp3';
-import shaved_stash from '../sounds/shaved_stash.mp3';
 import skim_milk from '../sounds/skim_milk.mp3';
 import swansons from '../sounds/swansons.mp3';
 import talk_back from '../sounds/talk_back.mp3';
-import teach_a_man_to_fish from '../sounds/teach_a_man_to_fish.mp3';
 import veganisim from '../sounds/veganisim.mp3';
 
 class Ron extends React.Component {
 	sounds = [
-		joke,
-		giggle,
-		breakfast,
-		at_doctor,
-		bacon_and_eggs,
-		beaurocrat,
-		born_ready,
-		cake,
-		capitalisim,
-		chummy,
-		currency,
-		dentist,
-		fish,
-		fishing_yoga,
+		veganisim,
 		frozen_yoghurt,
+		fish,
+		talk_back,
+		dentist,
+		beaurocrat,
+		fishing_yoga,
+		party_platter,
+		bacon_and_eggs,
+		at_doctor,
+		currency,
+		born_ready,
 		hangover,
+		joke,
+		cake,
+		giggle,
+		skim_milk,
+		chummy,
 		hernia,
 		honeymoon,
-		hostile_witness,
 		i_know_more_than_you,
-		no_steak,
-		party_platter,
 		ribs,
-		salad,
 		say_no,
-		shaved_stash,
-		skim_milk,
-		swansons,
-		talk_back,
-		teach_a_man_to_fish,
-		veganisim
+		swansons
 	];
 	state = {
 		status: 'STOPPED',
-		randomSound: '',
-		lastRandomSound: ''
+		sound: '',
+		soundCount: -1
 	};
 
 	handleClick = (e) => {
-		this.getRandomSoundEffect();
+		this.playSound();
 	};
 
-	getRandomSoundEffect = () => {
-		let randomSound = this.sounds[
-			Math.floor(Math.random() * this.sounds.length)
-		];
+	playSound = () => {
+		let i = this.state.soundCount;
+		console.log(this.sounds.length);
+		if (i < this.sounds.length - 1) {
+			i++;
 
-		if (randomSound === this.state.lastRandomSound) {
-			// randomSong
-			this.getRandomSoundEffect();
-		} else {
+			let sound = this.sounds[i];
+
 			this.setState({
-				randomSound: randomSound,
-				lastRandomSound: randomSound,
+				sound: sound,
+				soundCount: i,
 				status: 'PLAYING'
 			});
+		} else {
+			this.setState({ soundCount: -1, sound: '' });
 		}
 	};
 
 	render() {
-		const { status, randomSound } = this.state;
+		const { status, sound } = this.state;
 
 		return (
 			<div className='ron-header'>
@@ -114,11 +101,15 @@ class Ron extends React.Component {
 					onClick={this.handleClick}
 				/>
 
-				<SoundComponent status={status} soundEffect={randomSound} />
+				<SoundComponent status={status} soundEffect={sound} />
 				<footer>
 					Made with <img src={steak} className='steak' alt='steak' />{' '}
 					by <br />
-					<a href='https://www.darreninglis.com' target='_blank'>
+					<a
+						href='https://www.darreninglis.com'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
 						Darren Inglis
 					</a>
 				</footer>
